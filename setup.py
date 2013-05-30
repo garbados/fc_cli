3c5efe913f61c12a28329eba848aa41cb6e2c68b
Max Thayer <garbados@gmail.com>
2013-05-30 15:22:51 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='FC_CLI',
-      version='0.0.1',
+      version='0.0.2',
       description='Get a FullContact report on somebody, and store it in Cloudant',
       author='Max Thayer',
       author_email='garbados@gmail.com',
